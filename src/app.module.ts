bf6bec8eec4d01d87805e1374673b8e5a4ea91de
maycon-santana <115080830+maycon-santana@users.noreply.github.com>
2024-12-07 16:48:45 -0300

    Refatorando o modulo de configuracao de ambiente

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,14 @@
-import { Module } from '@nestjs/common'; 
+import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { EnvConfigModule } from './shared/infrastructure/env-config/env-config.module';
 
 @Module({
-  imports: [EnvConfigModule],
+  imports: [
+    EnvConfigModule.forRoot({
+      isGlobal: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
